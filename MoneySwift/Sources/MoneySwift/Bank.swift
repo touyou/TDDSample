b47e6a9ef50b7e2f114dc96c7a6af892dd9f8623
touyou <465697+touyou@users.noreply.github.com>
2025-11-01 23:44:12 +0900

    BankクラスのreduceメソッドをSumのreduceメソッドを呼び出すように変更。Sumクラスにreduceメソッドを実装し、加算の処理を移動。TODOリストにBank.reduceの項目を追加。

--- MoneySwift/Sources/MoneySwift/Bank.swift
+++ MoneySwift/Sources/MoneySwift/Bank.swift
@@ -1,7 +1,6 @@
 class Bank {
     func reduce(_ source: Expression, to: String) -> Money {
         let sum = source as! Sum
-        let amount = sum.augend._amount + sum.addend._amount
-        return Money(amount: amount, currency: to)
+        return sum.reduce(to: to)
     }
 }
--- MoneySwift/Sources/MoneySwift/Sum.swift
+++ MoneySwift/Sources/MoneySwift/Sum.swift
@@ -1,4 +1,9 @@
 struct Sum: Expression{
   let augend: Money
   let addend: Money
+
+  func reduce(to: String) -> Money {
+    let amount = augend._amount + addend._amount
+    return Money(amount: amount, currency: to)
+  }
 }
