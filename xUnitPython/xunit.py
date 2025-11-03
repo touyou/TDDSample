4267b68bd89aaa743a71b097f92dd79b09c71c18
touyou <465697+touyou@users.noreply.github.com>
2025-11-03 16:26:10 +0900

    TestCaseTestクラスを追加し、テストメソッドの実行を統一

--- xUnitPython/xunit.py
+++ xUnitPython/xunit.py
@@ -1,6 +1,11 @@
 from wasrun import WasRun
+from testcase import TestCase
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
