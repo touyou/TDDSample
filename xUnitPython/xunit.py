27812e4e5478506a5e0f441dd9b2ad540ed9a424
touyou <465697+touyou@users.noreply.github.com>
2025-11-03 16:39:13 +0900

    WasRunクラスのsetUpメソッドからwasSetUpの初期化を削除し、testMethod内でのログ更新を修正。TestCaseTestクラスのテストメソッドを整理し、testTemplateMethodに統一。

--- xUnitPython/xunit.py
+++ xUnitPython/xunit.py
@@ -13,23 +13,18 @@ class TestCase:
 class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
         self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
+    def testTemplateMethod(self):
         self.test.run()
-        assert(self.test.wasRun)
+        assert("setUp testMethod " == self.test.log)
 
-    def testSetUp(self):
-        self.test.run()
-        assert("setUp " == self.test.log)
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
