047dae284536cf639129760325d4b9ed30653ead
touyou <465697+touyou@users.noreply.github.com>
2025-11-03 16:41:42 +0900

    WasRunクラスとTestCaseクラスにtearDownメソッドを追加し、runメソッド内での呼び出しを実装。テストのログ出力を修正。

--- xUnitPython/xunit.py
+++ xUnitPython/xunit.py
@@ -5,10 +5,14 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
@@ -17,10 +21,13 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
 
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
